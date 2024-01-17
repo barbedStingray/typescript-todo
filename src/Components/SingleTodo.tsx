c3ebe529b3dd80dee37bb1d75f0c95dcd1a424e7
Ben Rehmann <brehmann@me.com>
2024-01-17 14:30:55 -0600

    todo list complete, could use more styling

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from '../model';
 import { PiPencilCircleFill } from "react-icons/pi";
 import { FiDelete, FiCheckCircle } from "react-icons/fi";
@@ -14,21 +14,40 @@ type Props = {
 
 const SingleTodo = ({ todo, todos, setTodos, }: Props) => {
 
+    // edit states
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
     // task complete
     const handleDone = (id: number) => {
         // map and select 
         setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
     }
-
     // delete task
     const handleDelete = (id: number) => {
         setTodos(todos.filter((todo) => todo.id !== id));
     }
+    // edit task
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+
+        setTodos(todos.map((todo) => (
+            todo.id === id ? {...todo, todo:editTodo } : todo
+        )))
+        setEdit(false);
+    }
+
 
 
     return (
-        <form className='todos_single'>
-            {
+        <form className='todos_single' onSubmit={(e) => handleEdit(e, todo.id)}>
+            {edit ? (
+                <input 
+                    value={editTodo} 
+                    onChange={(e) => setEditTodo(e.target.value)}
+                    className='todos_single--text'
+                />
+            ) : (
                 todo.isDone ? (
                     <s className='todos_single--text'>
                         {todo.todo}
@@ -38,10 +57,16 @@ const SingleTodo = ({ todo, todos, setTodos, }: Props) => {
                         {todo.todo}
                     </span>
                 )
-            }
+            )}
+
+            {/* rendering complete or not */}
 
             <div>
-                <span className="icon">
+                <span className="icon" onClick={() => {
+                    if (!edit && !todo.isDone) {
+                        setEdit(!edit);
+                    }
+                }}>
                     <PiPencilCircleFill />
                 </span>
                 <span className="icon" onClick={() => handleDelete(todo.id)}>
